3e186d08562aa9e92ed4092c20aebba132b2e3a4
Jacob Owen <jacob.owen@sabay.com>
2021-01-19 22:39:05 +0700

    WIP Initial data store model

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,7 +10,27 @@ module.exports = () => {
   });
 
   router.post('/', (request, response) => {
-      return response.json({title: 'New job'});
+      const entity = {
+        key: datastore.key('job'),
+        excludeFromIndexes: [
+          'description',
+          'created',
+          'updated',
+        ],
+        data: {
+          name: 'A Job',
+          description: 'Job description',
+          workingHours: '9-5',
+          salary: '$15/hour',
+          location: 'job location',
+          industry: 'job industry',
+          created: new Date(),
+          updated: new Date(),
+        },
+      };
+      datastore.insert(entity).then((err, apiResponse) => {
+        return response.json(apiResponse);
+      });
   });
 
   return router;
