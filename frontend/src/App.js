823d2a0a1d3debc2d867b705bed28709b0fa02e7
Jacob Owen <jacob.owen@sabay.com>
2021-02-11 22:37:40 +0700

    Bring data to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null)
+  
+  useEffect(() => {
+    fetch(`api/jobs`)
+    .then((response) => response.json())
+    .then(setData);
+  }, []);
+
+  if(data) {
+    return (
+      <div>
+        <h1>Latest Jobs</h1>
+        <ul>
+          {data.map((job) => (
+            <li>
+              <div>{job.name}</div>
+              <div>{job.location}</div>
+              <div><a href={'jobs/'+job.id}>Apply</a></div>
+            </li>
+          ))}
+        </ul>
+      </div>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Fetching jobs</div>
   );
 }
 
