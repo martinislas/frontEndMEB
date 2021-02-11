9c0154aed3090c5dc633fe964a108af2bf56461b
Jacob Owen <jacob.owen@sabay.com>
2021-02-11 21:15:30 +0700

    Fix dirname for ES6

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { fileURLToPath } from "url";
 import gcloudstore from '@google-cloud/datastore';
 
 import routes from './routes/index.js';
 
 const app = express();
-
 const datastore = new gcloudstore.Datastore();
 const port = process.env.PORT || 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname,'build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes({ datastore }));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build','index.html'));
 });
 
 app.use((error, request, response, next) => {
