fe7f0f9a87f5bcc0076348e664dca0d6032f6900
Owen Jacob <owen@donut.app>
2021-09-23 20:11:23 +0200

    Improve deployment

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,13 @@ const datastore = new gcloudstore.Datastore();
 const port = process.env.PORT || 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname,'build')));
+// app.use(express.static(path.join(__dirname,'build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes({ datastore }));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build','index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build','index.html'));
+// });
 
 app.use((error, request, response, next) => {
   const err = {
