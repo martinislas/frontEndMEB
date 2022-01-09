f799f0e797d4209922b702ed5746631f5572aca7
Owen Jacob <owen@donut.app>
2022-01-09 21:30:29 +0100

    Update axios call

--- frontend/src/components/AdminJobs.js
+++ frontend/src/components/AdminJobs.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useHistory } from 'react-router';
 import axios from 'axios';
 import 'bulma/css/bulma.min.css';
-import { Button, Container, Form, Level, Section } from 'react-bulma-components';
+import { Button, Container, Form, Section } from 'react-bulma-components';
 import useToken from '../auth/UseToken';
 
 function AdminJobs () {
@@ -16,67 +16,65 @@ function AdminJobs () {
   const onLoginClicked = async () => {
     try {
       const response = await axios.post('/api/jobs', {
-        headers: {'Authorization': 'Bearer ' + token},
         name: form.title,
         description: form.description,
         salary: form.salary,
         location: form.location,
         industry: form.industry,
-      });
-      if (response.status !== 200) {
-        history.push('/admin#jobs?status=failed');
-      }
+      }, {
+      headers: {'Authorization': 'Bearer ' + token}
+      })
       history.push('/admin#jobs?status=success');
       console.log(response)
     } catch (e) {
-      console.log(e)
+      if (e.response) {
+        history.push('/admin#jobs?status=failed');
+      } else {
+        console.log(e)
+      }
     }
   }
 
   return (
     <Section id="jobs">
       <Container>
-        <Level>
-          <Level.Item>
-            <div>
-              <Form.Field>
-                <Form.Label>Job Title</Form.Label>
-                <Form.Control>
-                  <Form.Input name="title" type="text" value={form.title} onChange={update} />
-                </Form.Control>
-              </Form.Field>
-              <Form.Field>
-                <Form.Label>Job Description</Form.Label>
-                <Form.Control>
-                  <Form.Input name="description" type="text" value={form.description} onChange={update} />
-                </Form.Control>
-              </Form.Field>
-              <Form.Field>
-                <Form.Label>Salary</Form.Label>
-                <Form.Control>
-                  <Form.Input name="salary" type="text" value={form.salary} onChange={update} />
-                </Form.Control>
-              </Form.Field>
-              <Form.Field>
-                <Form.Label>Location</Form.Label>
-                <Form.Control>
-                  <Form.Input name="location" type="text" value={form.location} onChange={update} />
-                </Form.Control>
-              </Form.Field>
-              <Form.Field>
-                <Form.Label>Industry</Form.Label>
-                <Form.Control>
-                  <Form.Input name="industry" type="text" value={form.industry} onChange={update} />
-                </Form.Control>
-              </Form.Field>
-              <Form.Field>
-                <Form.Control>
-                  <Button type="primary" onClick={onLoginClicked}>Submit</Button>
-                </Form.Control>
-              </Form.Field>
-            </div>
-          </Level.Item>
-        </Level>
+        <div>
+          <Form.Field>
+            <Form.Label>Job Title</Form.Label>
+            <Form.Control>
+              <Form.Input name="title" type="text" value={form.title} onChange={update} />
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>Job Description</Form.Label>
+            <Form.Control>
+              <Form.Input name="description" type="text" value={form.description} onChange={update} />
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>Salary</Form.Label>
+            <Form.Control>
+              <Form.Input name="salary" type="text" value={form.salary} onChange={update} />
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>Location</Form.Label>
+            <Form.Control>
+              <Form.Input name="location" type="text" value={form.location} onChange={update} />
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>Industry</Form.Label>
+            <Form.Control>
+              <Form.Input name="industry" type="text" value={form.industry} onChange={update} />
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <Form.Control>
+              <Button type="primary" onClick={onLoginClicked}>Submit</Button>
+            </Form.Control>
+          </Form.Field>
+        </div>
       </Container>
       <Container>Update Job ID</Container>
       <Container>Delete Job ID</Container>
